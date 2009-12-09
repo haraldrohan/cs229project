8cad65639097ab844a55b9575e4e6fe06dd1d9f3
huangty <huangty@85b52b7c-c905-11de-9e96-3193d784f102>
2009-12-09 08:43:38 +0000

    latest version

--- trunk/MoViSign/src/edu/stanford/movisign/MoViSign.java
+++ trunk/MoViSign/src/edu/stanford/movisign/MoViSign.java
@@ -1,11 +1,15 @@
 package edu.stanford.movisign;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
+import java.util.StringTokenizer;
+import java.lang.Double;
 
 import android.app.Activity;
 import android.app.Dialog;
@@ -31,20 +35,24 @@ public class MoViSign extends Activity {
     /** Called when the activity is first created. */
 	private SensorManager sensorMgr;
 	private TextView accuracyLabel;
-	private TextView xLabel, yLabel, zLabel, directionLabel, inclinationLabel, aboveBelowLabel, LogLabel;
+	//private TextView xLabel, yLabel, zLabel, directionLabel, inclinationLabel, aboveBelowLabel;
+	private TextView LogLabel, testResultLabel;
 	private boolean accelSupported;
    /* private Button startLogButton;
     private Button stopLogButton;*/
 	private Button NameOKButton = null;
 	private Button SignSuccButton = null;
-	private Button SignFailButton =null;
+	private Button SignFailButton = null;
+	private Button TestButton = null;
     private ToggleButton LogButton = null;
     
 	
     private List<Sensor> sensors;
-    private Sensor accSensor;
-    private float oldX, oldY, oldZ = 0f; 
+    @SuppressWarnings("unused")
+	private Sensor accSensor;
+    //private float oldX, oldY, oldZ = 0f; 
     private boolean logging = false;
+    public int NumOfCluster = 16;
     
   	public static volatile float kFilteringFactor = (float)0.05;
 	public static volatile float azimuth = (float) 0;
@@ -55,7 +63,7 @@ public class MoViSign extends Activity {
 	public static volatile float accelY = (float) 0;
 	public static volatile float accelZ = (float)0;
 
-	private File root = null;
+	//private File root = null;
 	private File OrientationTempLog = null;
 	private File AccelerometerTempLog = null;
 	private File MergedTempLog = null;
@@ -74,6 +82,9 @@ public class MoViSign extends Activity {
         }
 
         File logfile = new File(directory.getPath()+"/"+filename);
+        if(!logfile.exists()){
+        	logfile.delete();
+        }
         if (!logfile.exists() && directory.exists()){
                 try {
                     logfile.createNewFile();
@@ -84,17 +95,130 @@ public class MoViSign extends Activity {
         return logfile;
     }
 
-	
+	public boolean testSignature(){
+	/*	File directory = new File(Environment.getExternalStorageDirectory().getPath()+"/movisign");
+		BufferedReader log = null;
+		
+        if (!directory.exists()) {
+                return false;
+        }
+        File logfile = new File(directory.getPath()+"/Merged.temp");
+        try {
+			log = new BufferedReader(new FileReader(logfile));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        // TODO: get W & b
+		double W[] = new double[NumOfCluster*6];
+		double b = 0;
+		File trainResult = new File(directory.getPath()+"/trainResault.txt");
+		BufferedReader trainResultReader = null;
+		try {
+			trainResultReader = new BufferedReader(new FileReader(trainResult));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String line = null;
+		try {
+			line = trainResultReader.readLine();
+			if(line!=null){
+				b = (double) Double.valueOf(line);
+			}
+			line = trainResultReader.readLine();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int w = 0;
+		while(line != null){
+			StringTokenizer st = new StringTokenizer(line);
+			while (st.hasMoreTokens()) {
+		         W[w] = (double) Double.valueOf(st.nextToken());
+		    }
+			try {
+				line = trainResultReader.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		// Processing Testing Data
+		int NumOfLine = 0;
+		line = null;
+		try {
+			line = log.readLine();
+			while(line != null){
+				NumOfLine++;
+				line = log.readLine();
+			}
+			log.close();
+			log = new BufferedReader(new FileReader(logfile));
+			line = log.readLine();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		double testData[][] = new double[NumOfLine][7];
+		int lineNum =0;
+		while(line != null){
+			lineNum++;
+			StringTokenizer st = new StringTokenizer(line);
+			int i=0;
+			while (st.hasMoreTokens()) {
+		         testData[lineNum-1][i] = (double) Double.valueOf(st.nextToken());
+		    }
+			
+			try {
+				line = log.readLine();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	    int numInCluster = (int) Math.ceil((lineNum / NumOfCluster));
+	    int k = 0;
+	    double featureData[][] = new double[NumOfCluster][6];
+	    double X[] = new double[NumOfCluster*6];
+	    while (k < NumOfCluster){
+	    	for (int i = 0; i<6; i ++){
+	    		featureData[k][i] = 0;
+	    		int j = 0 ;
+	    		for(; j< numInCluster; j++){
+	    			lineNum = k*NumOfCluster + j;
+	    			if(lineNum < NumOfLine){
+	    				featureData[k][i] += testData[lineNum][i];
+	    			}else{
+	    				break;
+	    			}
+	    		}
+	    		featureData[k][i] /= j;
+	    	    X[k*6 + i] = featureData[k][i];
+	    	}
+	    	k++;
+	    }
+	    double answer = 0;
+	    for(int i = 0; i< NumOfCluster*6;i++){
+	    	answer += W[i]*X[i];
+	    }
+	    answer += b;
+        return (answer>0);*/
+		return true;
+	}
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         logging = false;
         setContentView(R.layout.main);
-        accuracyLabel = (TextView) findViewById(R.id.accuracyLabel);
+        /*accuracyLabel = (TextView) findViewById(R.id.accuracyLabel);
         xLabel = (TextView) findViewById(R.id.xLabel);
         yLabel = (TextView) findViewById(R.id.yLabel);
-        zLabel = (TextView) findViewById(R.id.zLabel);
+        zLabel = (TextView) findViewById(R.id.zLabel);*/
         LogLabel = (TextView) findViewById(R.id.LogLabel);
+        testResultLabel = (TextView) findViewById(R.id.TestResultLabel);
         /*startLogButton = (Button) findViewById(R.id.ButtonStartLog);
         startLogButton.setOnClickListener(new OnClickListener(){
         	public void onClick(View v) {
@@ -143,7 +267,20 @@ public class MoViSign extends Activity {
        		checkDialog.dismiss();
        	}
         });
-        
+    	
+    	TestButton = (Button) checkDialog.findViewById(R.id.ButtonTest);
+    	TestButton.setOnClickListener(new OnClickListener(){
+           	public void onClick(View v){
+           		/*boolean result = false;
+           		result = testSignature();
+           		if(result == true){
+           			testResultLabel.setText("True Signature");
+           		}else{
+           			testResultLabel.setText("False Signature");
+           		}*/
+           		checkDialog.dismiss();
+           	}
+        });
         LogButton = (ToggleButton) findViewById(R.id.LogButton);
         LogButton.setOnTouchListener(new OnTouchListener() {
             
@@ -171,9 +308,9 @@ public class MoViSign extends Activity {
                  return true;
             }
        }); 
-        directionLabel = (TextView) findViewById(R.id.directionLabel);
+        /*directionLabel = (TextView) findViewById(R.id.directionLabel);
         inclinationLabel = (TextView) findViewById(R.id.inclinationLabel);
-        aboveBelowLabel = (TextView) findViewById(R.id.aboveBelowLabel);
+        aboveBelowLabel = (TextView) findViewById(R.id.aboveBelowLabel);*/
         sensorMgr = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         sensors = sensorMgr.getSensorList(Sensor.TYPE_ACCELEROMETER);
         if(sensors.size() > 0)
